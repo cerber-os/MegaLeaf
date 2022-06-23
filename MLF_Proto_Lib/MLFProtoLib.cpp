53548e1ec33de600950da828a8d3fe76ac5889cd
Pawel Wieczorek <contact@cerberos.pl>
2022-06-23 18:14:09 +0200

    [Lib] Fix communication errors and Windows support

--- MLF_Proto_Lib/MLFProtoLib.cpp
+++ MLF_Proto_Lib/MLFProtoLib.cpp
@@ -1,15 +1,15 @@
 /**
  * @file MLFProtoLib.cpp
  * @author Pawel Wieczorek
- * @brief Userspace library for communiating with MegaLeaf controller
- * @version 0.1
+ * @brief Userspace library for communicating with MegaLeaf controller
  * @date 2022-06-13
- * 
  */
 #include "MLFProtoLib.hpp"
 #include "MLFProtoLib.h"
 
 #include <fcntl.h>
+#include "CRC.h"
+
 
 /************************************
  * PLATFORM SPECIFIC CODE
@@ -22,6 +22,9 @@
 #include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
+
+/* Binary open mode is only required on Windows */
+#define O_BINARY    0
 
 /* Const string identifier used by MLF controller during USB enumeration */
 #define SERIAL_ID_PREFIX        "usb-cerber-os_MegaLeaf_CDC_Controller_"
@@ -50,7 +53,7 @@ static std::string GetPathToUSBDevice(void) {
 
 static void ConfigureSerialPort(int fd) {
     struct termios tty;
-    if (tcgetattr (fd, &tty) != 0)
+    if(tcgetattr(fd, &tty) != 0)
         throw MLFException("failed to setup usb connection - get attrs", true);
 
     cfsetospeed(&tty, B1152000);
@@ -58,7 +61,7 @@ static void ConfigureSerialPort(int fd) {
 
     tty.c_lflag = 0;
     tty.c_oflag = 0;
-    tty.c_cc[VMIN]  = 1;
+    tty.c_cc[VMIN] = 1;
     tty.c_cc[VTIME] = 8;
 
     tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
@@ -66,7 +69,7 @@ static void ConfigureSerialPort(int fd) {
     tty.c_cflag |= (CLOCAL | CREAD | CS8);
     tty.c_cflag &= ~(PARENB | PARODD | CSTOPB | CRTSCTS);
 
-    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+    if(tcsetattr(fd, TCSANOW, &tty) != 0)
         throw MLFException("failed to setup usb connection - set attrs", true);
 }
 
@@ -108,11 +111,11 @@ static std::string GetPathToUSBDevice(void) {
         ret = SetupDiGetDeviceProperty(devInfoSet, &devInfoData,
             &DEVPKEY_Device_BusReportedDeviceDesc, &PropType, (PBYTE)buffer,
             sizeof(buffer), &size, 0);
-        if (!ret || PropType != DEVPROP_TYPE_STRING)
+        if(!ret || PropType != DEVPROP_TYPE_STRING)
             continue;
 
         // Ignore all devices except MLF Controller
-        if (wcscmp(buffer, L"MegaLeaf CDC Controller") != 0)
+        if(wcscmp(buffer, L"MegaLeaf CDC Controller") != 0)
             continue;
 
         // Extract Device Friendly Name
@@ -121,13 +124,13 @@ static std::string GetPathToUSBDevice(void) {
         ret = SetupDiGetDeviceProperty(devInfoSet, &devInfoData,
             &DEVPKEY_Device_FriendlyName, &PropType, (PBYTE)buffer,
             sizeof(buffer), &size, 0);
-        if (!ret || PropType != DEVPROP_TYPE_STRING) {
+        if(!ret || PropType != DEVPROP_TYPE_STRING) {
             goto exit;
         }
 
         size_t size;
         wcstombs_s(&size, friendlyName, buffer, sizeof(friendlyName));
-        if (size < sizeof(BUS_NAME_PREFIX) + 2) {
+        if(size < sizeof(BUS_NAME_PREFIX) + 2) {
             goto exit;
         }
 
@@ -136,14 +139,36 @@ static std::string GetPathToUSBDevice(void) {
     }
 
 exit:
-    if (devInfoSet)
+    if(devInfoSet)
         SetupDiDestroyDeviceInfoList(devInfoSet);
     return result;
 }
 
-/* On Windows there's no need for an additional configuration of serial port*/
+/* On Windows there's no need for additional configuration of serial COM port*/
 static void ConfigureSerialPort(int fd) {
-    (fd);
+    bool ret;
+    HANDLE serialPort = (HANDLE)_get_osfhandle(fd);
+
+    DCB dcb;
+    ret = GetCommState(serialPort, &dcb);
+    if(!ret)
+        throw MLFException("failed to setup usb connection - GetCommState", true);
+
+    dcb.BaudRate = CBR_115200;
+    dcb.ByteSize = 8;
+    dcb.StopBits = ONESTOPBIT;
+    dcb.Parity = NOPARITY;
+    dcb.fOutX = 0;
+    dcb.fInX = 0;
+    dcb.fNull = 0;
+    dcb.fDtrControl = DTR_CONTROL_DISABLE;
+    dcb.fRtsControl = RTS_CONTROL_DISABLE;
+
+    ret = SetCommState(serialPort, &dcb);
+    if(!ret)
+        throw MLFException("failed to setup usb connection - SetCommState", true);
+    
+    // TODO: Set timeouts using SetCommTimeouts
 }
 
 /* Nasty fix to overcome "Posix name deprecated" error */
@@ -179,7 +204,7 @@ void MLFProtoLib::_read(void* data, size_t len) {
     size_t alreadyRead = 0;
 
     while(alreadyRead < len) {
-        ret = read(dev, (char*) data + alreadyRead, len - alreadyRead);
+        ret = read(dev, (char*)data + alreadyRead, len - alreadyRead);
         if(ret < 0)
             throw MLFException("failed to read data from MLF Controller", true);
         else if(ret == 0)
@@ -194,7 +219,7 @@ void MLFProtoLib::_write(void* data, size_t len) {
     size_t alreadyWritten = 0;
 
     while(alreadyWritten < len) {
-        ret = write(dev, (char*) data + alreadyWritten, len - alreadyWritten);
+        ret = write(dev, (char*)data + alreadyWritten, len - alreadyWritten);
         if(ret <= 0)
             throw MLFException("failed to write data to MLF Controller", true);
         alreadyWritten += ret;
@@ -202,13 +227,11 @@ void MLFProtoLib::_write(void* data, size_t len) {
 }
 
 int MLFProtoLib::_calcCRC(void* data, int len) {
-    return 0;
-    // TODO:
+    return CRC::Calculate(data, len, CRC::CRC_32());
 }
 
 int MLFProtoLib::_appendCRC(int crc, void* data, int len) {
-    return 0;
-    // TODO:
+    return CRC::Calculate(data, len, CRC::CRC_32(), (uint32_t)crc);
 }
 
 /*
@@ -219,18 +242,18 @@ int MLFProtoLib::_appendCRC(int crc, void* data, int len) {
  *          FOOTER contains: CRC checksum of HEADER+BODY, magic
  */
 
-/**
- * @brief Send command with optional data from host to controller
- * 
- * @param cmd  ID of command to invoke on controller's side
- * @param data (optional) pointer to data sent with command
- * @param len  (optional) size of `data` array
- */
+ /**
+  * @brief Send command with optional data from host to controller
+  *
+  * @param cmd  ID of command to invoke on controller's side
+  * @param data (optional) pointer to data sent with command
+  * @param len  (optional) size of `data` array
+  */
 void MLFProtoLib::_sendData(int cmd, void* data, int len) {
     struct MLF_req_packet_header header = {
         .magic = MLF_HEADER_MAGIC,
-        .cmd = (uint8_t) cmd,
-        .data_size = (uint16_t) len
+        .cmd = (uint8_t)cmd,
+        .data_size = (uint16_t)len
     };
     struct MLF_packet_footer footer = {
         .magic = MLF_FOOTER_MAGIC
@@ -240,10 +263,12 @@ void MLFProtoLib::_sendData(int cmd, void* data, int len) {
     char* packet = new char[packet_size];
     memcpy(packet, &header, sizeof header);
     memcpy(packet + sizeof header, data, len);
-    
+
     // Calculate CRC of header + body and add it to packet footer
-    footer.crc = _calcCRC(packet, sizeof header + len);
+    int crcLen = (sizeof header + len) & 0xfffffffc;
+    footer.crc = _calcCRC(packet, crcLen);
     memcpy(packet + sizeof header + len, &footer, sizeof footer);
+
 
     _write(packet, packet_size);
     delete[] packet;
@@ -273,24 +298,23 @@ int MLFProtoLib::_recvData(void* output, int outputLen, int* actualLen) {
     _read(&header, sizeof(header));
     if(header.magic != MLF_HEADER_MAGIC)
         throw MLFException("invalid header magic was received from MLF Controller");
-    
+
     try {
         // Read data associated with packet
         data_buffer = new char[header.data_size];
         _read(data_buffer, header.data_size);
-        
+
         // Read footer
-        _read(&footer, sizeof(footer));    
+        _read(&footer, sizeof(footer));
         if(footer.magic != MLF_FOOTER_MAGIC)
             throw MLFException("invalid footer magic was received from MLF Controller");
 
         // Validate CRC checksum
         int received_crc = _calcCRC(&header, sizeof header);
         received_crc = _appendCRC(received_crc, data_buffer, header.data_size);
-        if(received_crc != (int) footer.crc)
+        if(received_crc != (int)footer.crc)
             ;
-            // TODO: Fix CRC mismatch
-            //    throw std::runtime_error("Incorrect CRC in packet received from MLF Controller");
+        //    TODO: throw std::runtime_error("Incorrect CRC in packet received from MLF Controller");
 
         // Copy result
         if(outputLen > header.data_size)
@@ -299,9 +323,10 @@ int MLFProtoLib::_recvData(void* output, int outputLen, int* actualLen) {
             *actualLen = header.data_size;
         if(output != nullptr)
             memcpy(output, data_buffer, outputLen);
-    } catch (...) {
+    }
+    catch (...) {
         delete[] data_buffer;
-        throw ;
+        throw;
     }
 
     delete[] data_buffer;
@@ -336,7 +361,7 @@ MLFProtoLib::MLFProtoLib(std::string path) {
     }
     device_name = path;
 
-    dev = open(path.c_str(), O_RDWR);
+    dev = open(path.c_str(), O_RDWR | O_BINARY);
     if(dev < 0)
         throw MLFException("failed to open MLF Controller device", true);
 
@@ -354,9 +379,10 @@ MLFProtoLib::MLFProtoLib(std::string path) {
         fw_version = resp.fw_version;
         leds_count_top = resp.leds_count_top;
         leds_count_bottom = resp.leds_count_bottom;
-    } catch(...) {
+    }
+    catch (...) {
         close(dev);
-        throw ;
+        throw;
     }
 }
 
@@ -380,7 +406,7 @@ void MLFProtoLib::turnOff(void) {
 
 void MLFProtoLib::setBrightness(int brightness) {
     struct MLF_req_cmd_set_brightness data = {
-        .brightness = (uint8_t) brightness,
+        .brightness = (uint8_t)brightness,
         .strip = 0b11
     };
 
@@ -394,15 +420,23 @@ void MLFProtoLib::setColors(int* colors, int len) {
 
     memcpy(data->colors, colors, len * sizeof(int));
 
-    invokeCmd(MLF_CMD_SET_COLOR, &data, dataLen);
+    try {
+        invokeCmd(MLF_CMD_SET_COLOR, data, dataLen);
+    }
+    catch (...) {
+        delete[] data;
+        throw;
+    }
+
+    delete[] data;
 }
 
 void MLFProtoLib::setEffect(int effect, int speed, int strip, int color) {
     struct MLF_req_cmd_set_effect data = {
-        .effect = (uint8_t) effect,
-        .speed = (uint8_t) speed,
-        .strip = (uint8_t) strip,
-        .color = (uint32_t) color,
+        .effect = (uint8_t)effect,
+        .speed = (uint8_t)speed,
+        .strip = (uint8_t)strip,
+        .color = (uint32_t)color,
     };
 
     invokeCmd(MLF_CMD_SET_EFFECT, &data, sizeof data);
@@ -417,15 +451,17 @@ void MLFProtoLib::setEffect(int effect, int speed, int strip, int color) {
 MLF_handler MLFProtoLib_Init(char* path) {
     try {
         return new MLFProtoLib(path);
-    } catch (...) {
+    }
+    catch (...) {
         return NULL;
     }
 }
 
 void MLFProtoLib_Deinit(MLF_handler handle) {
     try {
-        delete (MLFProtoLib*) handle;
-    } catch (...) {
+        delete (MLFProtoLib*)handle;
+    }
+    catch (...) {
         ;
     }
 }
@@ -435,7 +471,8 @@ void MLFProtoLib_GetFWVersion(MLF_handler handle, int* version) {
         int ver;
         MLF_PROTO_OBJ(handle)->getFWVersion(ver);
         *version = ver;
-    } catch (...) {
+    }
+    catch (...) {
         ;
     }
 }
@@ -446,7 +483,8 @@ void MLFProtoLib_GetLedsCount(MLF_handler handle, int* top, int* bottom) {
         MLF_PROTO_OBJ(handle)->getLedsCount(t, b);
         *top = t;
         *bottom = b;
-    } catch (...) {
+    }
+    catch (...) {
         ;
     }
 }
@@ -455,7 +493,8 @@ int MLFProtoLib_TurnOff(MLF_handler handle) {
     try {
         MLF_PROTO_OBJ(handle)->turnOff();
         return 0;
-    } catch (...) {
+    }
+    catch (...) {
         return -1;
     }
 }
@@ -464,7 +503,8 @@ int MLFProtoLib_SetBrightness(MLF_handler handle, int val) {
     try {
         MLF_PROTO_OBJ(handle)->setBrightness(val);
         return 0;
-    } catch (...) {
+    }
+    catch (...) {
         return -1;
     }
 }
@@ -473,7 +513,8 @@ int MLFProtoLib_SetColors(MLF_handler handle, int* colors, int len) {
     try {
         MLF_PROTO_OBJ(handle)->setColors(colors, len);
         return 0;
-    } catch (...) {
+    }
+    catch (...) {
         return -1;
     }
 }
@@ -482,7 +523,8 @@ int MLFProtoLib_SetEffect(MLF_handler handle, int effect, int speed, int strip, 
     try {
         MLF_PROTO_OBJ(handle)->setEffect(effect, speed, strip, color);
         return 0;
-    } catch (...) {
+    }
+    catch (...) {
         return -1;
     }
 }
